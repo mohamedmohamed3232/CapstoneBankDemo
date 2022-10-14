6c868f42c0f722815118a7bde25488b73e67ff44
mohamedmohamed3232 <44126576+mohamedmohamed3232@users.noreply.github.com>
2022-10-13 20:52:34 -0400

    Created the User Model

---
+++ src/main/java/com/mohamed/capstonebankdemo/CapstoneBankDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.capstonebankdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBankDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneBankDemoApplication.class, args);
+    }
+
+}
